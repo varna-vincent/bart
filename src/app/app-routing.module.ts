5bce49672a71e76bd4212b1f64c933c3e605908d
varna-vincent <vincent.varna@gmail.com>
2019-02-28 14:22:32 -0800

    Initial setup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { StationsComponent } from './components/stations/stations.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+	{ path: '', component: HomeComponent },
+    { path: 'stations', component: StationsComponent }
+    // { path: '', redirectTo: '/stations', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
+export const RoutingComponents = [ HomeComponent ]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, RoutingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule, Routes } from '@angular/router';
+import { MatToolbarModule } from '@angular/material';
+import { HomeComponent } from './components/home/home.component';
+import { StationsComponent } from './components/stations/stations.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RoutingComponents,
+    HomeComponent,
+    StationsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
