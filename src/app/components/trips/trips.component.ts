8a5258974b9cec9ae23395db4c7900b1eeac6056
varna-vincent <vincent.varna@gmail.com>
2019-03-10 18:22:02 -0700

    Reload every 30sec to get real time data

--- src/app/components/trips/trips.component.ts
+++ src/app/components/trips/trips.component.ts
@@ -15,11 +15,13 @@ export class TripsComponent implements OnInit {
   public stations: any = [];
   public station: any = [];
   public schedule: any = [];
+  intervalId: number;
 
   constructor(private stationsService: StationsService, private tripsService: TripsService) {} 
 
   ngOnInit() {
-  	this.loadStations()
+  	this.loadStations();
+    this.intervalId = setInterval(() => { this.getTrips(); this.getStationInfo(); }, 30000);
   }
 
    // Get stations list
@@ -32,18 +34,24 @@ export class TripsComponent implements OnInit {
 
   // Get trips list
   getTrips() {
-    return this.tripsService.getTrips(this.stationsSelected.source.abbr, this.stationsSelected.destination.abbr).subscribe((data: {}) => {
-      this.schedule = data;
-      console.log(data);
-    })
+    let source = this.stationsSelected.source;
+    let dest = this.stationsSelected.destination;
+    if(source && source.abbr && dest && dest.abbr && source.abbr !== dest.abbr) {
+      return this.tripsService.getTrips(source.abbr, dest.abbr).subscribe((data: {}) => {
+        this.schedule = data;
+        console.log(data);
+      })
+    }
   }
 
   // Get source station info
   getStationInfo() {
-    return this.stationsService.getStation(this.stationsSelected.source.abbr).subscribe((data: {}) => {
-      this.station = data;
-      console.log(data);
-    })
+    if(this.stationsSelected.source && this.stationsSelected.source.abbr) {
+      return this.stationsService.getStation(this.stationsSelected.source.abbr).subscribe((data: {}) => {
+        this.station = data;
+        console.log(data);
+      })
+    }
   }
 
 }
