0e75cd1007cd7759a34bce75526084fed1d58bda
varna-vincent <vincent.varna@gmail.com>
2019-03-10 17:00:42 -0700

    Localstorage to track visit count

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -6,10 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+	
+  visitCount: number;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
+
+  	this.visitCount = window.localStorage.getItem( 'visit_count');
+	window.localStorage.setItem( 'visit_count', this.visitCount === null ? '1' : parseInt(this.visitCount) + 1 + '');
   }
 
 }
